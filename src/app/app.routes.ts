e0a031b702e83b15bef3e0257f640d0ba89dbd91
maximiliankoch <tschaka1904@gmail.com>
2017-01-03 16:40:49 +0000

    [CP-68] Added route for home component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import {Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 export const rootRouterConfig: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'}
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
 ];
