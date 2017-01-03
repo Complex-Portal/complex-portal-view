f658dd6b24b10550d9ecec8f5f11315623d3c46f
maximiliankoch <tschaka1904@gmail.com>
2017-01-03 14:42:42 +0000

    [CP-59] Added model of complex-details. This model is used for the details page.

---
+++ src/app/complex/shared/model/complex-details/complex-details.model.ts
@@ -0,0 +1,169 @@
+import {Participant} from './participant.model';
+import {CrossReference} from './cross-reference.model';
+
+export class ComplexDetails {
+  private _ac:string;
+  private _name:string;
+  private _systematicName:string;
+  private _species:string;
+  private _institution:string;
+  private _synonyms:string[];
+  private _functions:string[];
+  private _properties:string[];
+  private _ligands:string[];
+  private _complexAssemblies:string[];
+  private _diseases:string[];
+  private _agonists:string[];
+  private _antagonists:string[];
+  private _comments:string[];
+  private _participants:Participant[];
+  private _crossReferences:CrossReference[];
+
+  constructor(ac:string, name:string, systematicName:string, species:string, institution:string, synonyms:string[], functions:string[], properties:string[], ligands:string[], complexAssemblies:string[], diseases:string[], agonists:string[], antagonists:string[], comments:string[], participants:Participant[], crossReferences:CrossReference[]) {
+    this._ac = ac;
+    this._name = name;
+    this._systematicName = systematicName;
+    this._species = species;
+    this._institution = institution;
+    this._synonyms = synonyms;
+    this._functions = functions;
+    this._properties = properties;
+    this._ligands = ligands;
+    this._complexAssemblies = complexAssemblies;
+    this._diseases = diseases;
+    this._agonists = agonists;
+    this._antagonists = antagonists;
+    this._comments = comments;
+    this._participants = participants;
+    this._crossReferences = crossReferences;
+  }
+
+
+  get ac(): string {
+    return this._ac;
+  }
+
+  set ac(value: string) {
+    this._ac = value;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get systematicName(): string {
+    return this._systematicName;
+  }
+
+  set systematicName(value: string) {
+    this._systematicName = value;
+  }
+
+  get species(): string {
+    return this._species;
+  }
+
+  set species(value: string) {
+    this._species = value;
+  }
+
+  get institution(): string {
+    return this._institution;
+  }
+
+  set institution(value: string) {
+    this._institution = value;
+  }
+
+  get synonyms(): string[] {
+    return this._synonyms;
+  }
+
+  set synonyms(value: string[]) {
+    this._synonyms = value;
+  }
+
+  get functions(): string[] {
+    return this._functions;
+  }
+
+  set functions(value: string[]) {
+    this._functions = value;
+  }
+
+  get properties(): string[] {
+    return this._properties;
+  }
+
+  set properties(value: string[]) {
+    this._properties = value;
+  }
+
+  get ligands(): string[] {
+    return this._ligands;
+  }
+
+  set ligands(value: string[]) {
+    this._ligands = value;
+  }
+
+  get complexAssemblies(): string[] {
+    return this._complexAssemblies;
+  }
+
+  set complexAssemblies(value: string[]) {
+    this._complexAssemblies = value;
+  }
+
+  get diseases(): string[] {
+    return this._diseases;
+  }
+
+  set diseases(value: string[]) {
+    this._diseases = value;
+  }
+
+  get agonists(): string[] {
+    return this._agonists;
+  }
+
+  set agonists(value: string[]) {
+    this._agonists = value;
+  }
+
+  get antagonists(): string[] {
+    return this._antagonists;
+  }
+
+  set antagonists(value: string[]) {
+    this._antagonists = value;
+  }
+
+  get comments(): string[] {
+    return this._comments;
+  }
+
+  set comments(value: string[]) {
+    this._comments = value;
+  }
+
+  get participants(): Participant[] {
+    return this._participants;
+  }
+
+  set participants(value: Participant[]) {
+    this._participants = value;
+  }
+
+  get crossReferences(): CrossReference[] {
+    return this._crossReferences;
+  }
+
+  set crossReferences(value: CrossReference[]) {
+    this._crossReferences = value;
+  }
+}
---
+++ src/app/complex/shared/model/complex-details/features.model.ts
@@ -0,0 +1,60 @@
+/**
+ * Created by mkoch on 19/07/2016.
+ */
+export class Features {
+  private _participantId:string;
+  private _featureType:string;
+  private _featureTypeMI:string;
+  private _featureTypeDefinition:string;
+  private _ranges:string[];
+
+
+  constructor(participantId:string, featureType:string, featureTypeMI:string, featureTypeDefinition:string, ranges:string[]) {
+      this._participantId = participantId;
+      this._featureType = featureType;
+      this._featureTypeMI = featureTypeMI;
+      this._featureTypeDefinition = featureTypeDefinition;
+          this._ranges = ranges;
+      }
+
+
+  get participantId(): string {
+    return this._participantId;
+  }
+
+  set participantId(value: string) {
+    this._participantId = value;
+  }
+
+  get featureType(): string {
+    return this._featureType;
+  }
+
+  set featureType(value: string) {
+    this._featureType = value;
+  }
+
+  get featureTypeMI(): string {
+    return this._featureTypeMI;
+  }
+
+  set featureTypeMI(value: string) {
+    this._featureTypeMI = value;
+  }
+
+  get featureTypeDefinition(): string {
+    return this._featureTypeDefinition;
+  }
+
+  set featureTypeDefinition(value: string) {
+    this._featureTypeDefinition = value;
+  }
+
+  get ranges(): string[] {
+    return this._ranges;
+  }
+
+  set ranges(value: string[]) {
+    this._ranges = value;
+  }
+}
