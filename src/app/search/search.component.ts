330aa3ee863ecd433003b4e28e355216f7482cde
maximiliankoch <tschaka1904@gmail.com>
2017-01-03 16:00:50 +0000

    [CP-61] Added method to submit search. For now this enough. But we need also some logic for the suggestion box and the auto-pre-search.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  search(query: string) {
+    this.router.navigate(['search'], { queryParams: { query: query }});
   }
 
 }
