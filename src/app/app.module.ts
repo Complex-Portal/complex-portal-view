89b9fe159b907e3c62a5c9970489025f46efaa51
maximiliankoch <tschaka1904@gmail.com>
2017-01-03 12:05:04 +0000

    [CP-54] Added router-module to list of used modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {RouterModule} from "@angular/router";
+import {rootRouterConfig} from './app.routes';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(rootRouterConfig),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
