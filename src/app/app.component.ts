fe0b675dd402aa10ff921055a4dbbc78348b0ebb
maximiliankoch <tschaka1904@gmail.com>
2017-01-12 14:54:44 +0000

    [CP-86] Added listener to app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {NotificationService} from "./shared/notification/service/notification.service";
 declare var $: any;
 
 @Component({
@@ -7,6 +8,31 @@ declare var $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+
+  constructor(private notificationService : NotificationService) {
+    notificationService.successNotification$.subscribe(
+      notification => {
+        console.log(notification);
+      });
+    notificationService.errorNotification$.subscribe(
+      notification => {
+        console.log(notification);
+      });
+    notificationService.announcementNotification$.subscribe(
+      notification => {
+        console.log(notification);
+      });
+    notificationService.hintNotification$.subscribe(
+      notification => {
+        console.log(notification);
+      });
+    notificationService.staticNotification$.subscribe(
+      notification => {
+        console.log(notification);
+      });
+  }
+
   ngOnInit(): void {
   }
 }
