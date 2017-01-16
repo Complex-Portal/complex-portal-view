06c3e299fd69fe29cea1747004f0eb08e05f4aac
maximiliankoch <tschaka1904@gmail.com>
2017-01-16 14:33:58 +0000

    [CP-91] Let's see if that works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, AfterContentInit} from '@angular/core';
 import {NotificationService} from './shared/notification/service/notification.service';
-import { Angulartics2GoogleAnalytics } from 'angulartics2';
+import {Angulartics2GoogleAnalytics} from 'angulartics2';
 import {environment} from '../environments/environment';
 declare var $: any;
+declare var ga: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterContentInit {
 
-  constructor(private notificationService: NotificationService,private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
+  constructor(private notificationService: NotificationService, private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
   }
 
   ngOnInit(): void {
     if (environment.production === false) {
       this.notificationService.addStaticNotification('Please be aware the you\'re currently looking a DEV version of the Complex Portal!');
     } else {
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', environment.analytics_id, 'none');
+      ga('create', 'UA-672146-11', 'none');
     }
+  }
+
+
+  ngAfterContentInit(): void {
   }
 }
