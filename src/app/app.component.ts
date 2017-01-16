0fe5449287f13283eb3a4991bf6ee8fb51da070d
maximiliankoch <tschaka1904@gmail.com>
2017-01-16 14:16:11 +0000

    [CP-91] Let's see if that works.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,13 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     if (environment.production === false) {
       this.notificationService.addStaticNotification('Please be aware the you\'re currently looking a DEV version of the Complex Portal!');
+    } else {
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', environment.analytics_id, 'none');
     }
   }
 }
