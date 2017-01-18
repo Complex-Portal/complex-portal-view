ed6c6677d6398f35e788a243b848cdaca35a6e61
maximiliankoch <tschaka1904@gmail.com>
2017-01-18 16:19:09 +0000

    [CP-94] Added function to retrieve complex details infortions.

--- src/app/complex/complex-details/complex-details.component.ts
+++ src/app/complex/complex-details/complex-details.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ComplexDetails} from "../shared/model/complex-details/complex-details.model";
+import {ActivatedRoute} from "@angular/router";
+import {ComplexPortalService} from "../shared/service/complex-portal.service";
 
 @Component({
   selector: 'app-complex-details',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComplexDetailsComponent implements OnInit {
 
-  constructor() { }
+  private _complexDetails: ComplexDetails;
+  private _complexMIJSON: any;
+  private _query: string;
 
-  ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private complexPortalService: ComplexPortalService) {
   }
 
+  ngOnInit(): void {
+    this.route
+      .params
+      .subscribe(params => {
+        this._query = params['id'];
+        this.complexPortalService.getComplex(this._query).subscribe(
+          complexDetails => this.complexDetails = complexDetails);
+        this.complexPortalService.getComplexMIJSON(this._query).subscribe(
+          complexMIJSON => this.complexMIJSON = complexMIJSON,);
+      });
+  }
+
+  get complexDetails(): ComplexDetails {
+    return this._complexDetails;
+  }
+
+  set complexDetails(value: ComplexDetails) {
+    this._complexDetails = value;
+  }
+
+  get complexMIJSON(): any {
+    return this._complexMIJSON;
+  }
+
+  set complexMIJSON(value: any) {
+    this._complexMIJSON = value;
+  }
+
+  get query(): string {
+    return this._query;
+  }
+
+  set query(value: string) {
+    this._query = value;
+  }
 }
