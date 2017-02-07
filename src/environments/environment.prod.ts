2b18ac18dfe09b1adcb48bed02d64c5d08e3d3be
maximiliankoch <tschaka1904@gmail.com>
2017-02-07 11:56:57 +0000

    [CP-94] use dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   analytics_id: 'UA-672146-12',
 
-  complex_ws: 'https://www.ebi.ac.uk/intact/complex-ws'
+  complex_ws: 'https://wwwdev.ebi.ac.uk/intact/complex-ws'
 };
