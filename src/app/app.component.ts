8ba6cc2cbd0dbb9b5cfc7af58cb829af34cf9072
maximiliankoch <tschaka1904@gmail.com>
2017-02-08 11:57:25 +0000

    [CP-94] Another attempt to bring all the scripts into angular. The reason is, that we can embed the mi-model.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,19 +13,38 @@ declare var ga: any;
 export class AppComponent implements OnInit, AfterContentInit {
 
   constructor(private angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
+
   }
 
   ngOnInit(): void {
+    $(document).foundation();
+    $(document).foundationExtendEBI();
     if (environment.production === false) {
       ga('create', environment.analytics_id, 'none');
     } else {
       ga('create', environment.analytics_id, 'none');
     }
+    //copied from script.js (ebi framework)
+    // Assign global nav background images through meta tags
+    (function assignImageByMetaTags() {
+      var localMasthead = document.getElementById('local-masthead');
+      // check for both ebi: and ebi- formatted meta tags
+      var localMastheadColor = document.querySelector("meta[name='ebi:localmasthead-color']") || document.querySelector("meta[name='ebi-localmasthead-color']");
+      var localMastheadImage = document.querySelector("meta[name='ebi:localmasthead-image']") || document.querySelector("meta[name='ebi-localmasthead-image']");
+      if (localMastheadColor != null) {
+        localMasthead.style.backgroundColor = localMastheadColor.getAttribute("content");
+        localMasthead.className += ' meta-background-color';
+      }
+      if (localMastheadImage != null) {
+        localMasthead.style.backgroundImage = 'url(' + localMastheadImage.getAttribute("content") + ')';
+        localMasthead.className += ' meta-background-image';
+      }
+    })();
   }
 
 
   ngAfterContentInit(): void {
-    $(document).foundation();
-    $(document).foundationExtendEBI();
+    // $(document).foundation();
+    // $(document).foundationExtendEBI();
   }
 }
