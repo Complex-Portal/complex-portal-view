62ef432a3b2b43bad5befdc8a98e6269688feedf
maximiliankoch <tschaka1904@gmail.com>
2017-02-22 11:28:28 +0000

    [CP-97] Changed layout of go xrefs. Based on issue #4 on GitHub

---
+++ src/app/complex/complex-details/complex-function/go-crossreferences/go-biological-process/go-biological-process.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {CrossReference} from "../../../../shared/model/complex-details/cross-reference.model";
+
+@Component({
+  selector: 'app-go-biological-process',
+  templateUrl: 'go-biological-process.component.html',
+  styleUrls: ['go-biological-process.component.css']
+})
+export class GoBiologicalProcessComponent implements OnInit {
+  private _crossReferences: CrossReference[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  get crossReferences(): CrossReference[] {
+    return this._crossReferences;
+  }
+
+  @Input()
+  set crossReferences(value: CrossReference[]) {
+    this._crossReferences = value;
+  }
+}
---
+++ src/app/complex/complex-details/complex-function/go-crossreferences/go-molecular-function/go-molecular-function.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit, Input} from '@angular/core';
+import {CrossReference} from "../../../../shared/model/complex-details/cross-reference.model";
+
+@Component({
+  selector: 'app-go-molecular-function',
+  templateUrl: 'go-molecular-function.component.html',
+  styleUrls: ['go-molecular-function.component.css']
+})
+export class GoMolecularFunctionComponent implements OnInit {
+  private _crossReferences: CrossReference[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  get crossReferences(): CrossReference[] {
+    return this._crossReferences;
+  }
+
+  @Input()
+  set crossReferences(value: CrossReference[]) {
+    this._crossReferences = value;
+  }
+}
