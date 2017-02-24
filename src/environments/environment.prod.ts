bbd12b35abceab530f56589c44eaf1259b801521
maximiliankoch <tschaka1904@gmail.com>
2017-02-24 09:22:59 +0000

    Silly to forget these.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
   analytics_id: 'UA-672146-12',
 
-  complex_ws_base_url: 'https://wwwdev.ebi.ac.uk/intact/complex-ws'
+  complex_ws_base_url: '//wwwdev.ebi.ac.uk/intact/complex-ws',
+  europepmc_base_url: '//www.ebi.ac.uk/europepmc',
+  reactome_base_url: '//reactomedev.oicr.on.ca',
+  pdb_base_url: '//www.ebi.ac.uk/pdbe',
 };
