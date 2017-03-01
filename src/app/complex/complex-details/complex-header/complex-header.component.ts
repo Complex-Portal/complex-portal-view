86611c552cd80ef9c866c4095b82065755a7008a
maximiliankoch <tschaka1904@gmail.com>
2017-03-01 10:15:12 +0000

    [CP-97] Added json button, based on issue #46 on GitHub

--- src/app/complex/complex-details/complex-header/complex-header.component.ts
+++ src/app/complex/complex-details/complex-header/complex-header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-complex-header',
@@ -7,13 +8,25 @@ import {Component, OnInit, Input} from '@angular/core';
 })
 export class ComplexHeaderComponent implements OnInit {
 
+  private _complexAC: string;
   private _complexName: string;
   private _complexSpecies: string;
+  private _jsonURL: string;
 
   constructor() {
   }
 
   ngOnInit() {
+    this._jsonURL = environment.complex_ws_base_url + '/details/' + this._complexAC;
+  }
+
+  get complexAC(): string {
+    return this._complexAC;
+  }
+
+  @Input()
+  set complexAC(value: string) {
+    this._complexAC = value;
   }
 
   get complexName(): string {
@@ -32,5 +45,13 @@ export class ComplexHeaderComponent implements OnInit {
   @Input()
   set complexSpecies(value: string) {
     this._complexSpecies = value;
+  }
+
+  get jsonURL(): string {
+    return this._jsonURL;
+  }
+
+  set jsonURL(value: string) {
+    this._jsonURL = value;
   }
 }
