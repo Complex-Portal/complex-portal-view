a22ddcf78f7805df2917a24c828de080e3b43fca
maximiliankoch <tschaka1904@gmail.com>
2017-03-20 13:57:28 +0000

    [CP-97] Added new route for basket

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {HomeComponent} from './home/home.component';
 import {ComplexResultsComponent} from './complex/complex-results/complex-results.component';
 import {DownloadComponent} from './download/download.component';
 import {ComplexDetailsComponent} from './complex/complex-details/complex-details.component';
+import {BasketComponent} from "./basket/basket.component";
 
 export const rootRouterConfig: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'download', component: DownloadComponent},
   {path: 'search', component: ComplexResultsComponent},
-  {path: 'complex/:id', component: ComplexDetailsComponent}
+  {path: 'complex/:id', component: ComplexDetailsComponent},
+  {path: 'basket', component: BasketComponent}
 ];
