2b33e4dc8b17b4f337bce7942b19a9cf175ebde7
maximiliankoch <tschaka1904@gmail.com>
2017-03-22 12:05:12 +0000

    [CP-97] Removed the contributors display.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {ProgressBarComponent} from "../shared/loading-indicators/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+    ProgressBarComponent.display();
     document.body.scrollTop = 0;
   }
 
+  ngAfterViewInit(): void {
+    ProgressBarComponent.hide();
+  }
 }
