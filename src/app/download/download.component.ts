49735a0406e10b2cdab72bc225afdb47719da93c
maximiliankoch <tschaka1904@gmail.com>
2017-03-22 11:57:46 +0000

    [CP-97] Added loader to see performance.

--- src/app/download/download.component.ts
+++ src/app/download/download.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {ProgressBarComponent} from "../shared/loading-indicators/progress-bar/progress-bar.component";
 
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.css']
 })
-export class DownloadComponent implements OnInit {
+export class DownloadComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+    ProgressBarComponent.display();
   }
 
+  ngAfterViewInit(): void {
+    ProgressBarComponent.hide();
+  }
 }
