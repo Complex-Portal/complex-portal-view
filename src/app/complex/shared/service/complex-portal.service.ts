2432124d7a24cfc19e035e30835dd6a46a3084e6
maximiliankoch <tschaka1904@gmail.com>
2017-04-12 10:09:27 +0100

    [CP-97] Use the right import!

--- src/app/complex/shared/service/complex-portal.service.ts
+++ src/app/complex/shared/service/complex-portal.service.ts
@@ -4,6 +4,7 @@ import {environment} from '../../../../environments/environment';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 const baseURL = environment.complex_ws_base_url;
 
