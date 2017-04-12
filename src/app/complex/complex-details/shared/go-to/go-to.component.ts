c18b86edff949cc8b8c0c605fbbac174b8846b11
maximiliankoch <tschaka1904@gmail.com>
2017-04-12 09:55:11 +0100

    [CP-97] New go-to component.

---
+++ src/app/complex/complex-details/shared/go-to/go-to.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-go-to',
+  templateUrl: './go-to.component.html',
+  styleUrls: ['./go-to.component.css']
+})
+export class GoToComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
