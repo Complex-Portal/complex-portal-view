7ab92faf756d277321d9f4d4709e74a003c7a275
maximiliankoch <tschaka1904@gmail.com>
2017-05-11 10:03:05 +0100

    [CP-97] Added google event for capturing the searches on the home page.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {Angulartics2} from "angulartics2";
 
 @Component({
   selector: 'cp-search',
@@ -8,13 +9,14 @@ import {Router} from '@angular/router';
 })
 export class SearchComponent implements OnInit {
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private angulartics2: Angulartics2) {
   }
 
   ngOnInit() {
   }
 
   search(query: string) {
-    this.router.navigate(['search'], { queryParams: { query: query, page: 1 }});
+    this.angulartics2.eventTrack.next({ action: query, properties: { category: 'search', label: 'home'}});
+    this.router.navigate(['complex/search'], { queryParams: { query: query, page: 1 }});
   }
 }
