e2160c27d1e545d5e06aea70bffceb4e5b6ac78e
maximiliankoch <tschaka1904@gmail.com>
2017-05-11 11:08:03 +0100

    [CP-97] ng lint

--- src/app/complex/complex-results/complex-results.module.ts
+++ src/app/complex/complex-results/complex-results.module.ts
@@ -1,13 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {ComplexResultsComponent} from "./complex-results.component";
-import {RouterModule} from "@angular/router";
-import {ComplexPaginatorComponent} from "./complex-paginator/complex-paginator.component";
-import {ComplexFilterComponent} from "./complex-filter/complex-filter.component";
-import {ComplexListComponent} from "./complex-list/complex-list.component";
-import {MdProgressSpinnerModule} from "@angular/material";
-import {ProgressSpinnerComponent} from "../../shared/loading-indicators/progress-spinner/progress-spinner.component";
-import {ComplexModule} from "app/complex/complex.module";
+import {ComplexResultsComponent} from './complex-results.component';
+import {RouterModule} from '@angular/router';
+import {ComplexPaginatorComponent} from './complex-paginator/complex-paginator.component';
+import {ComplexFilterComponent} from './complex-filter/complex-filter.component';
+import {ComplexListComponent} from './complex-list/complex-list.component';
+import {ComplexModule} from 'app/complex/complex.module';
 
 @NgModule({
   imports: [RouterModule.forChild([
