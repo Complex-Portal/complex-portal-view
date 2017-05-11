f9b295d221fba9f762b53e7c6fbdfceb6c9e350b
maximiliankoch <tschaka1904@gmail.com>
2017-05-11 10:48:22 +0100

    [CP-97] Let's inherit from the parent module

--- src/app/complex/complex-organisms/complex-organisms.module.ts
+++ src/app/complex/complex-organisms/complex-organisms.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import {ComplexOrganismComponent} from "./complex-organism/complex-organism.component";
 import {ComplexOrganismsComponent} from "./complex-organisms.component";
 import {RouterModule} from "@angular/router";
+import {MdProgressSpinnerModule} from "@angular/material";
+import {ProgressSpinnerComponent} from "../../shared/loading-indicators/progress-spinner/progress-spinner.component";
+import {ComplexModule} from "../complex.module";
 
 @NgModule({
   imports: [RouterModule.forChild([
     {path: '', component: ComplexOrganismsComponent}
   ]),
-    CommonModule
+    CommonModule,
+    ComplexModule
   ],
   declarations: [ComplexOrganismsComponent,
     ComplexOrganismComponent]
