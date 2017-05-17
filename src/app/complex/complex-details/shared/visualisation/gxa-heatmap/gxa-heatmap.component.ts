70abbde196710697432e8b12c06b0598ca8fd4e6
maximiliankoch <tschaka1904@gmail.com>
2017-05-17 16:29:40 +0100

    [CP-97] New gxa heatmap and also now with specific experiments.

--- src/app/complex/complex-details/shared/visualisation/gxa-heatmap/gxa-heatmap.component.ts
+++ src/app/complex/complex-details/shared/visualisation/gxa-heatmap/gxa-heatmap.component.ts
@@ -1,11 +1,6 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {Participant} from '../../../../shared/model/complex-details/participant.model';
 
-interface GXAQueryParams {
-  value: string;
-  category: string;
-}
-
 @Component({
   selector: 'cp-gxa-heatmap',
   templateUrl: 'gxa-heatmap.component.html',
@@ -15,23 +10,48 @@ export class GxaHeatmapComponent implements OnInit {
   private _gxa;
   private _participants: Participant[];
   private _complexSpecies: string;
-  private _gxaParamsQueries: GXAQueryParams[];
+  private _gxaParamsQueries: string;
   private _isLoaded = true;
+  private _experimentId: string;
 
   constructor() {
   }
 
   ngOnInit() {
-    const context = this;
-    const query: string = JSON.stringify(this._gxaParamsQueries) + '&species=' + this._complexSpecies.split(';')[0].toLowerCase();
-    this.gxa.render({
-      params: 'geneQuery=' + query,
-      isMultiExperiment: false,
-      target: 'heatmapContainer',
-      fail: function () {
-        context._isLoaded = false;
-      }
-    });
+    switch (this._complexSpecies) {
+      case 'Homo sapiens; 9606':
+        this._experimentId = 'E-MTAB-5214';
+        break;
+      case 'Mus musculus; 10090':
+        this._experimentId = 'E-MTAB-4644';
+        break;
+      case 'Rattus norvegicus; 10116':
+        this._experimentId = 'E-GEOD-53960';
+        break;
+      // case 'Gallus gallus (Chicken); 9031':
+      //   this._experimentId = 'E-MTAB-2797';
+      //   break;
+      case 'Caenorhabditis elegans; 6239':
+        this._experimentId = 'E-MTAB-2812';
+        break;
+      default:
+        this._isLoaded = false;
+    }
+
+    if(this._experimentId){
+      let context = this;
+      this._gxa.render({
+        target: 'highchartsContainer',
+        experiment: this._experimentId,
+        atlasUrl: 'https://wwwdev.ebi.ac.uk/gxa/',
+        query: {
+          gene: this.gxaParamsQueries
+        },
+        fail: function() {
+          context._isLoaded = false;
+        }
+      });
+    }
   }
 
   get gxa() {
@@ -61,12 +81,12 @@ export class GxaHeatmapComponent implements OnInit {
     this._complexSpecies = value;
   }
 
-  get gxaParamsQueries(): GXAQueryParams[] {
+  get gxaParamsQueries(): string {
     return this._gxaParamsQueries;
   }
 
   @Input()
-  set gxaParamsQueries(value: GXAQueryParams[]) {
+  set gxaParamsQueries(value: string) {
     this._gxaParamsQueries = value;
   }
 
