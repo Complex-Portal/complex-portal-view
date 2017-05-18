faf6e8352071f790da2dd2f9021e74c3410f55bd
maximiliankoch <tschaka1904@gmail.com>
2017-05-18 16:01:34 +0100

    [CP-97] A more modular approach.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,25 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
-export const environment = {
+const EBI_BASE_URL = '//wwwdev.ebi.ac.uk/';
+const INTACT_FTP_BASE_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/complex/';
+
+
+  export const environment = {
   production: false,
   evn: 'dev',
 
   analytics_id: 'UA-672146-11',
-  complex_ws_base_url: '//wwwdev.ebi.ac.uk/intact/complex-ws',
-  europepmc_base_url: '//www.ebi.ac.uk/europepmc',
+  ebi_base_url: EBI_BASE_URL,
+  complex_ws_base_url: EBI_BASE_URL + 'intact/complex-ws',
+  europepmc_base_url: EBI_BASE_URL + 'europepmc',
+  pdb_base_url: EBI_BASE_URL + 'pdbe',
+  ols_base_url: EBI_BASE_URL + 'ols/api/ontologies',
+  ols_go_url: EBI_BASE_URL + 'ols/ontologies/go/terms?obo_id=',
+  ols_eco_url: EBI_BASE_URL+ 'ols/ontologies/eco/terms?obo_id=',
   reactome_base_url: '//reactomedev.oicr.on.ca',
-  pdb_base_url: '//www.ebi.ac.uk/pdbe',
-  ols_base_url: '//www.ebi.ac.uk/ols/api/ontologies',
-  ols_go_url: '//www.ebi.ac.uk/ols/ontologies/go/terms?obo_id=',
-  ols_eco_url: '//www.ebi.ac.uk/ols/ontologies/eco/terms?obo_id='
+  intact_base_ftp: 'ftp://ftp.ebi.ac.uk/pub/databases/intact/complex/',
+  complex_tab: INTACT_FTP_BASE_URL + 'current/complextab/',
+  complex_psi25: INTACT_FTP_BASE_URL + 'current/psi25/',
+  complex_psi30: INTACT_FTP_BASE_URL+ 'current/psi30/'
 };
