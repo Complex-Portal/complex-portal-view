823ca98a20b5cbf174831e81c9a761b5a5ccfba3
maximiliankoch <tschaka1904@gmail.com>
2017-05-23 11:42:03 +0100

    [CP-97] Clean up

--- src/app/complex/complex-results/complex-results.module.ts
+++ src/app/complex/complex-results/complex-results.module.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ComplexPaginatorComponent } from './complex-paginator/complex-paginator.component';
 import { ComplexFilterComponent } from './complex-filter/complex-filter.component';
 import { ComplexListComponent } from './complex-list/complex-list.component';
-import { ProgressSpinnerModule } from "../../shared/loading-indicators/progress-spinner/progress-spinner.module";
+import { ProgressSpinnerModule } from '../../shared/loading-indicators/progress-spinner/progress-spinner.module';
 
 @NgModule({
   imports: [RouterModule.forChild([
