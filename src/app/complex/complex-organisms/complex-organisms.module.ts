d25fe4b9cbe7a3c788efb6d4046059c6c7e3dc9c
maximiliankoch <tschaka1904@gmail.com>
2017-05-23 10:13:41 +0100

    [CP-97] ' not "

--- src/app/complex/complex-organisms/complex-organisms.module.ts
+++ src/app/complex/complex-organisms/complex-organisms.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {ComplexOrganismComponent} from './complex-organism/complex-organism.component';
 import {ComplexOrganismsComponent} from './complex-organisms.component';
 import {RouterModule} from '@angular/router';
-import {ProgressSpinnerModule} from "../../shared/loading-indicators/progress-spinner/progress-spinner.module";
+import {ProgressSpinnerModule} from '../../shared/loading-indicators/progress-spinner/progress-spinner.module';
 
 @NgModule({
   imports: [RouterModule.forChild([
