511da87ef48b7b906ae8c15e5eba0c1c1308eb2a
maximiliankoch <tschaka1904@gmail.com>
2017-06-01 16:36:15 +0100

    [CP-97] Updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,32 @@
+/**
+ * Created by Maximilian Koch (mkoch@ebi.ac.uk) on 01/06/2017.
+ */
+
+//Enivorment for production
+const EBI_BASE_URL = '//www.ebi.ac.uk/';
+const REACTOME_BASE_URL = '//reactomedev.org';
+const INTACT_FTP_BASE_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/';
+const COMPLEX__FTP_BASE_URL = INTACT_FTP_BASE_URL + 'complex/';
+
 export const environment = {
   production: true,
-  analytics_id: 'UA-672146-12',
-  evn: '',
+  evn: 'prod',
 
-  complex_ws_base_url: '//wwwdev.ebi.ac.uk/intact/complex-ws',
-  europepmc_base_url: '//www.ebi.ac.uk/europepmc',
-  reactome_base_url: '//reactomedev.oicr.on.ca',
-  pdb_base_url: '//www.ebi.ac.uk/pdbe',
-  ols_base_url: '//www.ebi.ac.uk/ols/api/ontologies',
+  analytics_id: 'UA-672146-12',
+  ebi_base_url: EBI_BASE_URL,
+  complex_ws_base_url: EBI_BASE_URL + 'intact/complex-ws',
+  europepmc_base_url: EBI_BASE_URL + 'europepmc',
+  pdb_base_url: EBI_BASE_URL + 'pdbe',
+  ols_base_url: EBI_BASE_URL + 'ols/api/ontologies',
+  ols_go_url: EBI_BASE_URL + 'ols/ontologies/go/terms?obo_id=',
+  ols_eco_url: EBI_BASE_URL + 'ols/ontologies/eco/terms?obo_id=',
+  complex_current: COMPLEX__FTP_BASE_URL + 'current',
+  complex_tab: COMPLEX__FTP_BASE_URL + 'current/complextab/',
+  complex_tab_readme: COMPLEX__FTP_BASE_URL + 'current/complextab/README.htm',
+  complex_psi25: COMPLEX__FTP_BASE_URL + 'current/psi25/',
+  complex_psi30: COMPLEX__FTP_BASE_URL + 'current/psi30/',
+  intact_base_url: EBI_BASE_URL + 'intact',
+  intact_support_url: EBI_BASE_URL + 'support/intact',
+  intact_training_url: EBI_BASE_URL + 'intact/resources/training',
+  reactome_base_url: REACTOME_BASE_URL,
 };
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,19 +1,18 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
+/**
+ * Created by Maximilian Koch (mkoch@ebi.ac.uk) on 01/06/2017.
+ */
 
-// Enviorment for localhost
-const EBI_BASE_URL = '//wwwdev.ebi.ac.uk/';
-const REACTOME_BASE_URL = '//reactomedev.oicr.on.ca';
+// Enviorment for GitHub Page
+const EBI_BASE_URL = '//www.ebi.ac.uk/';
+const REACTOME_BASE_URL = '//reactomedev.org';
 const INTACT_FTP_BASE_URL = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/';
 const COMPLEX__FTP_BASE_URL = INTACT_FTP_BASE_URL + 'complex/';
 
 export const environment = {
-  production: false,
-  evn: 'dev',
+  production: true,
+  evn: 'test',
 
-  analytics_id: 'UA-672146-11',
+  analytics_id: 'UA-672146-13',
   ebi_base_url: EBI_BASE_URL,
   complex_ws_base_url: EBI_BASE_URL + 'intact/complex-ws',
   europepmc_base_url: EBI_BASE_URL + 'europepmc',
@@ -21,7 +20,6 @@ export const environment = {
   ols_base_url: EBI_BASE_URL + 'ols/api/ontologies',
   ols_go_url: EBI_BASE_URL + 'ols/ontologies/go/terms?obo_id=',
   ols_eco_url: EBI_BASE_URL + 'ols/ontologies/eco/terms?obo_id=',
-  reactome_base_url: REACTOME_BASE_URL,
   complex_current: COMPLEX__FTP_BASE_URL + 'current',
   complex_tab: COMPLEX__FTP_BASE_URL + 'current/complextab/',
   complex_tab_readme: COMPLEX__FTP_BASE_URL + 'current/complextab/README.htm',
@@ -30,4 +28,5 @@ export const environment = {
   intact_base_url: EBI_BASE_URL + 'intact',
   intact_support_url: EBI_BASE_URL + 'support/intact',
   intact_training_url: EBI_BASE_URL + 'intact/resources/training',
+  reactome_base_url: REACTOME_BASE_URL,
 };
