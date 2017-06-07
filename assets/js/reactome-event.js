0c47b5a13de661a2be60bc01bd723a2dd25b9fef
maximiliankoch <tschaka1904@gmail.com>
2017-06-07 09:44:29 +0100

    Auto-generated commit

--- assets/js/reactome-event.js
+++ assets/js/reactome-event.js
@@ -8,9 +8,8 @@ try {
 
     // var a = document.createElement('a');
     if (navigator.userAgent.indexOf('MSIE') !== -1 || navigator.appVersion.indexOf('Trident/') > 0) {
-      event = document.createEventObject();
-      event.eventType = "onReactomeDiagramReady";
-      form.fireEvent("onReactomeDiagramReady", {detail: this.Reactome});
+      var evt = document.createEvent("onReactomeDiagramReady");
+      evt.initCustomEvent('onReactomeDiagramReady', false, false, {detail: this.Reactome})
     } else {
       window.dispatchEvent(new CustomEvent('onReactomeDiagramReady', {detail: this.Reactome}));
     }
