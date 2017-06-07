353146b7fdda6e91c8d648d872ac484fa01fc393
maximiliankoch <tschaka1904@gmail.com>
2017-06-07 09:30:33 +0100

    Auto-generated commit

--- assets/js/reactome-event.js
+++ assets/js/reactome-event.js
@@ -4,7 +4,17 @@
 
 try {
   function onReactomeDiagramReady() {
-    window.dispatchEvent(new CustomEvent('onReactomeDiagramReady', {detail: this.Reactome}));
+    // window.dispatchEvent(new CustomEvent('onReactomeDiagramReady', {detail: this.Reactome}));
+
+    // var a = document.createElement('a');
+    if (navigator.userAgent.indexOf('MSIE') !== -1
+      || navigator.appVersion.indexOf('Trident/') > 0) {
+      var evt = document.createEvent('onReactomeDiagramReady');
+      evt.initEvent({detail: this.Reactome});
+      window.dispatchEvent(evt)
+    } else {
+      window.dispatchEvent(new CustomEvent('onReactomeDiagramReady', {detail: this.Reactome}));
+    }
   }
 } catch (ignore) {
 
