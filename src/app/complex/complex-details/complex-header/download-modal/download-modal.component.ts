3ca7764b607e63cc7b7f1e68727ac2df5015f7df
maximiliankoch <tschaka1904@gmail.com>
2017-06-29 11:03:11 +0100

    Added download modal to offer all the different download options. For GH issue #141

---
+++ src/app/complex/complex-details/complex-header/download-modal/download-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cp-download-modal',
+  templateUrl: './download-modal.component.html',
+  styleUrls: ['./download-modal.component.css']
+})
+export class DownloadModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
