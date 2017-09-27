afbd01783ecd08e30f79658b6078278adf54f808
maximiliankoch <tschaka1904@gmail.com>
2017-09-27 14:12:48 +0100

    Deal with error, make notification work again and throw event for error to GA.

--- src/app/complex/complex-details/complex-references/euro-pmc-crossreferences/euro-pmc-crossreferences.component.ts
+++ src/app/complex/complex-details/complex-references/euro-pmc-crossreferences/euro-pmc-crossreferences.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {CrossReference} from '../../../shared/model/complex-details/cross-reference.model';
 import {EuroPmcService} from './service/euro-pmc.service';
 import {NotificationService} from '../../../../shared/notification/service/notification.service';
+import {GoogleAnalyticsService} from '../../../../shared/google-analytics/service/google-analytics.service';
+import {Category} from '../../../../shared/google-analytics/category.enum';
 
 
 interface Publication {
@@ -24,36 +26,38 @@ export class EuroPmcCrossreferencesComponent implements OnInit {
   private _isDataLoaded = false;
 
 
-  constructor(private euroPmcService: EuroPmcService, private notificationService: NotificationService) {
+  constructor(private euroPmcService: EuroPmcService, private notificationService: NotificationService,
+              private googleAnalyticsService: GoogleAnalyticsService) {
   }
 
   ngOnInit() {
-    try {
-      for (let i = 0; i < this.crossReferences.length; i++) {
-        this.euroPmcService.getPublicationInformation(this.crossReferences[i].identifier).subscribe(
-          euroPmcResponse => this.publicationFactory(this.crossReferences[i], euroPmcResponse));
-        if (i === this.crossReferences.length - 1) {
-          this._isDataLoaded = true;
-        }
+    for (let i = 0; i < this.crossReferences.length; i++) {
+      this.euroPmcService.getPublicationInformation(this.crossReferences[i].identifier).subscribe(
+        euroPmcResponse => this.publicationFactory(this.crossReferences[i], euroPmcResponse),
+        error => this.onError(error)
+      );
+      if (i === this.crossReferences.length - 1) {
+        this._isDataLoaded = true;
       }
-    } catch (error) {
-      this._isDataLoaded = false;
-      this.notificationService.addErrorNotification('Error whilst retrieving data from Euro PMC. Please let us know if error persists.');
+    }
+  }
+
+  private onError(error: any) {
+    this._isDataLoaded = false;
+    if (error.status) {
+      this.notificationService.addErrorNotification('Error whilst retrieving data from Euro PMC. ' +
+        'Please let us know if error persists.');
+      this.googleAnalyticsService.fireAPIRequestErrorEvent(Category.europepmc, error.status);
     }
   }
 
   private publicationFactory(crossReference: CrossReference, euroPmcResponse: any): void {
-    try {
-      this.publications.push({
-        id: crossReference.identifier,
-        title: euroPmcResponse.resultList.result[0].title,
-        authors: euroPmcResponse.resultList.result[0].authorString,
-        url: crossReference.searchURL
-      });
-    } catch (error) {
-      this._isDataLoaded = false;
-      this.notificationService.addErrorNotification('Error whilst retrieving data from Euro PMC. Please let us know if error persists.');
-    }
+    this.publications.push({
+      id: crossReference.identifier,
+      title: euroPmcResponse.resultList.result[0].title,
+      authors: euroPmcResponse.resultList.result[0].authorString,
+      url: crossReference.searchURL
+    });
   }
 
   get crossReferences(): CrossReference[] {
