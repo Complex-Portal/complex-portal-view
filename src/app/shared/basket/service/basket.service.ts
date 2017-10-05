ad0e77de65f3e7ec44c4ee2820ffd37644a21a87
maximiliankoch <tschaka1904@gmail.com>
2017-10-05 16:11:58 +0100

    Clean up.

--- src/app/shared/basket/service/basket.service.ts
+++ src/app/shared/basket/service/basket.service.ts
@@ -3,7 +3,7 @@ import {BasketItem} from '../model/basketItem';
 import {Md5} from 'ts-md5/dist/md5';
 import {NotificationService} from '../../notification/service/notification.service';
 import {GoogleAnalyticsService} from '../../google-analytics/service/google-analytics.service';
-import {LocalStorageService} from '../../local-storage/local-storage.service';
+import {LocalStorageService} from '../../local-storage/service/local-storage.service';
 
 const COMPLEX_STORE = 'cp_complex_store';
 
