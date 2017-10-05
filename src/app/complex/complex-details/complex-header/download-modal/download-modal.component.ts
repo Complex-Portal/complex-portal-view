ac3b1147c0cc8fbefe56b3a48fb22ba6ebbde7fc
maximiliankoch <tschaka1904@gmail.com>
2017-10-05 16:29:10 +0100

    Clean up.

--- src/app/complex/complex-details/complex-header/download-modal/download-modal.component.ts
+++ src/app/complex/complex-details/complex-header/download-modal/download-modal.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {environment} from '../../../../../environments/environment';
-import {Category} from '../../../../shared/google-analytics/category.enum';
+import {Category} from '../../../../shared/google-analytics/types/category.enum';
 import {GoogleAnalyticsService} from '../../../../shared/google-analytics/service/google-analytics.service';
 
 @Component({
