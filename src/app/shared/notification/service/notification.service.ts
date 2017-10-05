a6adb610453c131cdfb9d7c1664e676290b728e8
maximiliankoch <tschaka1904@gmail.com>
2017-10-05 16:08:04 +0100

    Created methods for each case, so we don't have to much text all over the place. Better maintainable. Also added config to the service.

--- src/app/shared/notification/service/notification.service.ts
+++ src/app/shared/notification/service/notification.service.ts
@@ -1,10 +1,16 @@
 import {Injectable} from '@angular/core';
-import {ToastrService} from 'ngx-toastr';
+import {ToastrConfig, ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class NotificationService {
 
-  constructor(private toastrService: ToastrService) {
+  constructor(private toastrService: ToastrService, private toastrConfig: ToastrConfig) {
+    toastrConfig.closeButton = true; // displayedElements close button
+    toastrConfig.timeOut = 5000; // time to live
+    toastrConfig.preventDuplicates = true;
+    toastrConfig.progressBar = true;
+    toastrConfig.tapToDismiss = false;
+    toastrConfig.enableHtml = true;
   }
 
   private addSuccessNotification(successNotification: string): void {
@@ -28,15 +34,15 @@ export class NotificationService {
       'Please try again later or contact us if issue persists!');
   }
 
-  public onAddedComplexToBasket (id : string) {
-    this.addSuccessNotification('Stored ' + id + ' in your basket!')
+  public onAddedComplexToBasket(id: string) {
+    this.addSuccessNotification('Stored ' + id + ' in your basket!');
   }
 
-  public onRemovedComplexFromBasket (id : string) {
-    this.addSuccessNotification('Removed ' + id + ' in your basket!')
+  public onRemovedComplexFromBasket(id: string) {
+    this.addSuccessNotification('Removed ' + id + ' in your basket!');
   }
 
   public onFeatureNotAvailableYet() {
-    this.addHintNotification('This feature is not available yet. But it is coming soon! :-)')
+    this.addHintNotification('This feature is not available yet. But it is coming soon! :-)');
   }
 }
