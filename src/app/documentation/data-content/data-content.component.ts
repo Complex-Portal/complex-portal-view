6e736cab2f8b2c83331fb65fc98847111841321c
ntoro <ntoro@ebi.ac.uk>
2020-04-22 11:28:27 +0100

    Changes data-content documentation to use github as input and make it independent of the app deployment.

--- src/app/documentation/data-content/data-content.component.ts
+++ src/app/documentation/data-content/data-content.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {ProgressBarComponent} from '../../shared/loading-indicators/progress-bar/progress-bar.component';
-import {Router} from '@angular/router';
-import {environment} from '../../../environments/environment';
 import {Title} from '@angular/platform-browser';
+import {ProgressBarComponent} from '../../shared/loading-indicators/progress-bar/progress-bar.component';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'cp-data-content',
@@ -11,30 +10,13 @@ import {Title} from '@angular/platform-browser';
 })
 export class DataContentComponent implements OnInit {
 
-  constructor(private router: Router, private titleService: Title) {
+  source = environment.complex_portal_documentation_url + 'cp_content.md';
+
+  constructor(private titleService: Title) {
   }
 
   ngOnInit(): void {
     this.titleService.setTitle('Complex Portal - Data Content');
     ProgressBarComponent.hide();
-  }
-
-  public goToComplex(ac: string): void {
-    this.router.navigate(['complex', ac]);
-  }
-
-  public goToECO(term: string): void {
-    this.goTo(environment.ols_eco_url + term);
-  }
-
-  public goToGO(term: string): void {
-    this.goTo(environment.ols_go_url + term);
-  }
-
-  private goTo(url: string): void {
-    window.open(
-      url,
-      '_blank'
-    );
   }
 }
