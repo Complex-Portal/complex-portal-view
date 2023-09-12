30bdce7a9b67928d8a45ff3de58cfa0e58eda75e
jmedinaebi <jmedina@ebi.ac.uk>
2023-09-12 10:29:39 +0100

    Update components

--- src/app/complex/complex-details/complex-details.module.ts
+++ src/app/complex/complex-details/complex-details.module.ts
@@ -3,9 +3,8 @@ import {CommonModule} from '@angular/common';
 import {LitmolViewerComponent} from './shared/visualisation/litmol-viewer/litmol-viewer.component';
 import {PdbCrossreferencesComponent} from './complex-properties/pdb-crossreferences/pdb-crossreferences.component';
 import {EuroPmcCrossreferencesComponent} from './complex-references/euro-pmc-crossreferences/euro-pmc-crossreferences.component';
-import {IntenzCrossreferencesComponent} from './complex-function/intenz-crossreferences/intenz-crossreferences.component';
+import {CatalyticActivityComponent} from './complex-function/catalytic-activity/catalytic-activity.component';
 import {GoCrossreferencesComponent} from './complex-function/go-crossreferences/go-crossreferences.component';
-import {RheaCrossreferencesComponent} from './complex-function/rhea-crossreferences/rhea-crossreferences.component';
 import {ComplexPropertiesComponent} from './complex-properties/complex-properties.component';
 import {ComplexFunctionComponent} from './complex-function/complex-function.component';
 import {ComplexHeaderComponent} from './complex-header/complex-header.component';
@@ -66,8 +65,7 @@ import {ComplexModule} from '../complex.module';
     ReactomeDiagramComponent,
     ReactomeCrossReferencesComponent,
     GoCrossreferencesComponent,
-    IntenzCrossreferencesComponent,
-    RheaCrossreferencesComponent,
+    CatalyticActivityComponent,
     EuroPmcCrossreferencesComponent,
     PdbCrossreferencesComponent,
     LitmolViewerComponent,
---
+++ src/app/complex/complex-details/complex-function/catalytic-activity/catalytic-activity.component.ts
@@ -0,0 +1,59 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {CrossReference} from '../../../shared/model/complex-details/cross-reference.model';
+import {RheaCrossReference} from '../../../shared/model/complex-details/rhea-cross-reference.model';
+
+@Component({
+  selector: 'cp-catalytic-activity',
+  templateUrl: './catalytic-activity.component.html',
+  styleUrls: ['./catalytic-activity.component.css']
+})
+export class CatalyticActivityComponent implements OnInit {
+  private _intenzCrossReferences: CrossReference[];
+  private _rheaCrossReferences: RheaCrossReference[];
+  private _displayedIntenzElements = 5;
+  private _displayedRheaElements = 5;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  get intenzCrossReferences(): CrossReference[] {
+    return this._intenzCrossReferences;
+  }
+
+  @Input()
+  set intenzCrossReferences(value: CrossReference[]) {
+    this._intenzCrossReferences = value;
+  }
+
+  get rheaCrossReferences(): RheaCrossReference[] {
+    return this._rheaCrossReferences;
+  }
+
+  @Input()
+  set rheaCrossReferences(value: RheaCrossReference[]) {
+    this._rheaCrossReferences = value;
+  }
+
+  get displayedIntenzElements(): number {
+    return this._displayedIntenzElements;
+  }
+
+  set displayedIntenzElements(value: number) {
+    this._displayedIntenzElements = value;
+  }
+
+  get displayedRheaElements(): number {
+    return this._displayedRheaElements;
+  }
+
+  set displayedRheaElements(value: number) {
+    this._displayedRheaElements = value;
+  }
+
+  toggleRheaDisplayedProperty(index: number): void {
+    this._rheaCrossReferences[index].displayed = !this._rheaCrossReferences[index].displayed;
+  }
+}
