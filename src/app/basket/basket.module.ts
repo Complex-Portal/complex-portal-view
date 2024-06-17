e872d8fae79c261dd34d64669a6ac70989d7d186
Susie <hugetsusie@gmail.com>
2024-06-17 16:20:59 +0100

    Fixing on display issues for Firefox Retrieving of the complexes data for the basket Making the complex navigator more responsive for the size of the display

--- src/app/basket/basket.module.ts
+++ src/app/basket/basket.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {BasketComponent} from './basket.component';
+import {ComplexResultsModule} from '../complex/complex-results/complex-results.module';
 
 @NgModule({
   imports: [RouterModule.forChild([
