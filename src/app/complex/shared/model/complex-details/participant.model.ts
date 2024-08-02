7bbd129406648fceddf8d4df79b259c57d2896d1
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2024-08-02 16:19:22 +0100

    :recycle: Simplify models into interfaces + add predicted attribute

--- src/app/complex/shared/model/complex-details/participant.model.ts
+++ src/app/complex/shared/model/complex-details/participant.model.ts
@@ -1,141 +1,18 @@
 /**
  * Created by mkoch on 19/07/2016.
  */
-export class Participant {
-  private _interactorAC: string;
-  private _identifier: string;
-  private _identifierLink: string;
-  private _name: string;
-  private _description: string;
-  private _stochiometry: string;
-  private _bioRole: string;
-  private _bioRoleMI: string;
-  private _bioRoleDefinition: string;
-  private _interactorType: string;
-  private _interactorTypeMI: string;
-  private _interactorTypeDefinition: string;
-  private _interactorColour: string;
-
-  constructor(interactorAC: string, identifier: string, identifierLink: string, name: string, description: string,
-              stochiometry: string, bioRole: string, bioRoleMI: string, bioRoleDefinition: string,
-              interactorType: string, interactorTypeMI: string, interactorTypeDefinition: string) {
-    this._interactorAC = interactorAC;
-    this._identifier = identifier;
-    this._identifierLink = identifierLink;
-    this._name = name;
-    this._description = description;
-    this._stochiometry = stochiometry;
-    this._bioRole = bioRole;
-    this._bioRoleMI = bioRoleMI;
-    this._bioRoleDefinition = bioRoleDefinition;
-    this._interactorType = interactorType;
-    this._interactorTypeMI = interactorTypeMI;
-    this._interactorTypeDefinition = interactorTypeDefinition;
-    this._interactorColour = '#b02e2e'; // Default colour
-  }
-
-  get interactorAC(): string {
-    return this._interactorAC;
-  }
-
-  set interactorAC(value: string) {
-    this._interactorAC = value;
-  }
-
-  get identifier(): string {
-    return this._identifier;
-  }
-
-  set identifier(value: string) {
-    this._identifier = value;
-  }
-
-  get identifierLink(): string {
-    return this._identifierLink;
-  }
-
-  set identifierLink(value: string) {
-    this._identifierLink = value;
-  }
-
-  get name(): string {
-    return this._name;
-  }
-
-  set name(value: string) {
-    this._name = value;
-  }
-
-  get description(): string {
-    return this._description;
-  }
-
-  set description(value: string) {
-    this._description = value;
-  }
-
-  get stochiometry(): string {
-    return this._stochiometry;
-  }
-
-  set stochiometry(value: string) {
-    this._stochiometry = value;
-  }
-
-  get bioRole(): string {
-    return this._bioRole;
-  }
-
-  set bioRole(value: string) {
-    this._bioRole = value;
-  }
-
-  get bioRoleMI(): string {
-    return this._bioRoleMI;
-  }
-
-  set bioRoleMI(value: string) {
-    this._bioRoleMI = value;
-  }
-
-  get bioRoleDefinition(): string {
-    return this._bioRoleDefinition;
-  }
-
-  set bioRoleDefinition(value: string) {
-    this._bioRoleDefinition = value;
-  }
-
-  get interactorType(): string {
-    return this._interactorType;
-  }
-
-  set interactorType(value: string) {
-    this._interactorType = value;
-  }
-
-  get interactorTypeMI(): string {
-    return this._interactorTypeMI;
-  }
-
-  set interactorTypeMI(value: string) {
-    this._interactorTypeMI = value;
-  }
-
-  get interactorTypeDefinition(): string {
-    return this._interactorTypeDefinition;
-  }
-
-  set interactorTypeDefinition(value: string) {
-    this._interactorTypeDefinition = value;
-  }
-
-
-  get interactorColour(): string {
-    return this._interactorColour;
-  }
-
-  set interactorColour(value: string) {
-    this._interactorColour = value;
-  }
+export interface Participant {
+  interactorAC: string;
+  identifier: string;
+  identifierLink: string;
+  name: string;
+  description: string;
+  stochiometry: string;
+  bioRole: string;
+  bioRoleMI: string;
+  bioRoleDefinition: string;
+  interactorType: string;
+  interactorTypeMI: string;
+  interactorTypeDefinition: string;
+  interactorColour: string;
 }
--- src/app/complex/shared/model/complex-results/facets.model.ts
+++ src/app/complex/shared/model/complex-results/facets.model.ts
@@ -1,27 +1,9 @@
-import {Facet} from './facets/facet.model';
+import {Facet} from './facet.model';
 /**
  * Created by mkoch on 31/10/2016.
  */
-export class Facets {
-  private _ptype_f: Facet;
-  private _species_f: Facet;
-  private _pbiorole_f: Facet;
-
-  constructor(ptype_f: Facet, species_f: Facet, pbiorole_f: Facet) {
-    this._ptype_f = ptype_f;
-    this._species_f = species_f;
-    this._pbiorole_f = pbiorole_f;
-  }
-
-  get ptype_f(): Facet {
-    return this._ptype_f;
-  }
-
-  get species_f(): Facet {
-    return this._species_f;
-  }
-
-  get pbiorole_f(): Facet {
-    return this._pbiorole_f;
-  }
+export interface Facets {
+  ptype_f: Facet;
+  species_f: Facet;
+  pbiorole_f: Facet;
 }
