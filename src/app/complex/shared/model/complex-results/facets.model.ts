e51e975ff767c8fa2f11385d8ea38b1994c3757a
jmedinaebi <jmedina@ebi.ac.uk>
2024-08-06 10:57:40 +0100

    Update facet types wrongly set

--- src/app/complex/shared/model/complex-results/facets.model.ts
+++ src/app/complex/shared/model/complex-results/facets.model.ts
@@ -3,7 +3,7 @@ import {Facet} from './facet.model';
  * Created by mkoch on 31/10/2016.
  */
 export interface Facets {
-  ptype_f: Facet;
-  species_f: Facet;
-  pbiorole_f: Facet;
+  ptype_f: Facet[];
+  species_f: Facet[];
+  pbiorole_f: Facet[];
 }
