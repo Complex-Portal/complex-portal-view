b9e974154bf5a9c149a7f5fed33aff63dc991179
jmedinaebi <jmedina@ebi.ac.uk>
2024-10-11 12:43:29 +0100

    Show loading spinner while processing the components

--- src/app/complex/complex-results/complex-navigator/complex-navigator.component.ts
+++ src/app/complex/complex-results/complex-navigator/complex-navigator.component.ts
@@ -25,7 +25,11 @@ export class ComplexNavigatorComponent implements AfterViewInit {
   anyChange = output<void>();
 
   numberOfColumns = computed(() => this.complexSearch().elements.length);
-  navigatorComponentsWithoutGrouping = computed(() => this.createNavigatorComplexes(this.complexSearch().elements, this.interactors()));
+  navigatorComponentsWithoutGrouping = computed(() => {
+    // We set navigatorComponents as an empty list here to show the loading spinner while creating the components and ortholog groups
+    this.navigatorComponents = [];
+    this.createNavigatorComplexes(this.complexSearch().elements, this.interactors());
+  });
   navigatorComponentsGroupedByOrthologs = computed(() => this.createOrthologGroups(this.navigatorComponentsWithoutGrouping()));
   orthologGroupsAvailable = computed(() => this.navigatorComponentsGroupedByOrthologs().some(c => c instanceof NavigatorOrthologGroup));
 
